795ff5282c1a19d242ac70647eb3623d73373ae4
Khant Kyaw <khantkankei@gmail.com>
2023-12-05 22:47:41 +0630

    :sparkles: Add .env file in git

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(express.json({ limit: '50mb' })); // allow most 50mb for json data
 app.use(express.urlencoded({ extended: false })); // this line allow to take data from form data ma yone yin shrr kyie :3
 
 app.use('/api', router);
+app.use('/', (req, res) => {
+	res.send('Hello from the server');
+});
 
 app.use(errorMiddleware);
 
