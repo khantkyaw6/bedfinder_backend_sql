a2fe4b4185ddb5860636a1c2b2f5761b4e09b792
Khant Kyaw <khantkankei@gmail.com>
2023-12-05 23:42:53 +0630

    db config change

--- src/config/admindb.config.js
+++ src/config/admindb.config.js
@@ -4,12 +4,19 @@ const { DB_CONNECTION, DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD } =
 
 module.exports = {
 	development: {
-		username: DB_USERNAME || 'root',
-		password: DB_PASSWORD || 'root',
-		database: DB_DATABASE || 'database_development',
-		host: DB_HOST || '127.0.0.1',
+		username: DB_USERNAME || 'uoetu7izzjdtfi0x',
+		password: DB_PASSWORD || 'yP2NupbwkyQkCGiYupr4',
+		database: DB_DATABASE || 'bcrozjfxwwcy4x3gcxmf',
+		host: DB_HOST || 'bcrozjfxwwcy4x3gcxmf-mysql.services.clever-cloud.com',
 		dialect: DB_CONNECTION || 'mysql',
 	},
+	// development: {
+	// 	username: DB_USERNAME || 'root',
+	// 	password: DB_PASSWORD || 'root',
+	// 	database: DB_DATABASE || 'database_development',
+	// 	host: DB_HOST || '127.0.0.1',
+	// 	dialect: DB_CONNECTION || 'mysql',
+	// },
 	test: {
 		username: 'root',
 		password: null,
